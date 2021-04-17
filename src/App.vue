3acefb7a12bb9ced3b4885a4a969803d3ac92879
Brian Love <22353962+brianlove@users.noreply.github.com>
2021-04-17 01:43:19 -0400

    Convert to TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+// import HelloWorld from './components/HelloWorld.vue';
 import MinesweeperGame from '@/components/MinesweeperGame.vue';
 
-export default {
-  name: 'App',
+@Options({
   components: {
     MinesweeperGame,
-  }
-}
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   <MinesweeperGame />
 </template>
 
